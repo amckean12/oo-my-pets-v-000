eacf1c2cd11f41759a5f22a44a2d19115ce93bc4
Alex McKean <amckean@me.com>
2017-11-25 16:32:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,17 @@
 class Owner
+  @@all = []
 
   attr_accessor :name
   attr_reader :species
 
   def initialize(name)
     @name = name
+    @@all << @name
   end
+
+  def self.all 
+    @@all 
+  end 
 
   def species
     @species= "human"
