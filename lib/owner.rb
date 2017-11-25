6bca3e353c40e1f97ca828c26ca40d1e45b20727
Alex McKean <amckean@me.com>
2017-11-25 16:23:21 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,8 +3,9 @@ class Owner
   attr_accessor :name
   attr_reader :species
 
-  def initialize(name)
+  def initialize(name, species)
     @name = name
+    @species = species
   end
 
   def species(species)
