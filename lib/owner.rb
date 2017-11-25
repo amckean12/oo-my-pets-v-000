6efa85785224e456d23f159b719ea91ee06a08fe
Alex McKean <amckean@me.com>
2017-11-25 16:12:59 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,13 @@
 class Owner
   @@all = []
+  @@count = 0
 
   attr_accessor :name
 
   def initialize(name)
     @name = name
     @@all << @name
+    @@count += 1
   end
 
   def self.all
@@ -13,6 +15,6 @@ class Owner
   end
 
   def self.reset_all
-    @@all = []
+    @@count = 0
   end 
 end
